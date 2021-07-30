ccd52f494cd940accf6e3f334b32f85efc825e9a
ThorH <thor.haubert@hotmail.com>
2021-07-30 14:35:45 -0300

    ajustes da modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,20 +1,22 @@
-export default function Modal(){
+// export default function Modal(){
 
     const modalWrapper = document.querySelector('.modal-wrapper')
     const cancelButton = document.querySelector('.button.cancel')
+    const openButton = document.querySelector('.login .signup')
 
     cancelButton.addEventListener("click", close)
+    openButton.addEventListener("click", open)
 
     function open(){
         modalWrapper.classList.add("active")
     }
 
     function close(){
-        document.querySelector('.modal-wrapper').classList.remove("active")
+        modalWrapper.classList.remove("active")
     }
 
-    return{
-        open,
-        close
-    }
-}
+    // return{
+    //     open,
+    //     close
+    // }
+// }
