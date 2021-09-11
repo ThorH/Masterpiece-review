61c758932c254687c6b071484c41cc8da75d8ae3
ThorH <thor.haubert@hotmail.com>
2021-09-11 14:24:14 -0300

    Server comments

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ const server = express()
 const routes = require("./routes")
 const path = require("path")
 
+// usando o template engine
 server.set('view engine', 'ejs')
 
+// configura e muda a localização da pasta view 
 server.set('views', path.join(__dirname, 'views'))
 
+// Habilita os arquivos da pasta public
 server.use(express.static("public"))
 
-
+// 
 server.use(routes)
 
 server.listen(3000, () => console.log("rodando"))
